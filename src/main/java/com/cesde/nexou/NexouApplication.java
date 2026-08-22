a370631d5764051b8351d64d110b5f55bed8387e
SantiagoMJaramillo <santiagomjaramillo@outlook.com>
2026-08-22 12:45:12 -0500

    Creacion proyecto en Spring

---
+++ src/main/java/com/cesde/nexou/NexouApplication.java
@@ -0,0 +1,13 @@
+package com.cesde.nexou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexouApplication.class, args);
+	}
+
+}
