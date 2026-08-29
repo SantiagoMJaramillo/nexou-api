bb81e4c70b0fb56b57cebb20d26c69618d5448b3
Mariana <marianagv2222@gmail.com>
2026-08-29 08:44:09 -0500

    Completa entidad Categoria

--- src/main/java/com/cesde/nexou/model/entity/Categoria.java
+++ src/main/java/com/cesde/nexou/model/entity/Categoria.java
@@ -1,19 +1,27 @@
 package com.cesde.nexou.model.entity;
 
 import com.cesde.nexou.model.base.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name = "Categoria")
+@Table(name = "categorias")
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Categoria extends BaseEntity {
-    
+
+    @Column(nullable = false, unique = true, length = 80)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    @ManyToMany(mappedBy = "categorias")
+    private Set<Libro> libros = new HashSet<>();
 }
