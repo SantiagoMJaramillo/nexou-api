c7862d69e96937f654e05444ce4330a02d6ff8e6
Mariana <marianagv2222@gmail.com>
2026-08-29 08:43:09 -0500

    Completa entidad Usuario con sus campos y relación OneToOne

--- src/main/java/com/cesde/nexou/model/entity/Usuario.java
+++ src/main/java/com/cesde/nexou/model/entity/Usuario.java
@@ -1,19 +1,38 @@
 package com.cesde.nexou.model.entity;
 
 import com.cesde.nexou.model.base.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "usuarios")
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Usuario extends BaseEntity {
-    // Integrante B: agregar campos y relación @OneToOne con ConfiguracionUsuario
+
+    @Column(name = "nombre_usuario", nullable = false, length = 100)
+    private String nombreUsuario;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String correo;
+
+    @Column(nullable = false, length = 255)
+    private String contrasena;
+
+    @Column(name = "codigo_estudiante", length = 20)
+    private String codigoEstudiante;
+
+    @Column(length = 100)
+    private String programa;
+
+    private Integer semestre;
+
+    @Column(length = 20)
+    private String telefono;
+
+    @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private ConfiguracionUsuario configuracionUsuario;
 }
